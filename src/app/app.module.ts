1801a63f8623b5f38886679a53b4f145bdce8579
zeka <jevgenij.t@gmail.com>
2017-08-25 16:24:56 +0300

    type ahead

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import {BrowserModule} from "@angular/platform-browser";
 import {NgModule} from "@angular/core";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {HttpModule} from "@angular/http";
+import {HttpModule, JsonpModule} from "@angular/http";
 import {AppComponent} from "./app.component";
 import {TeamComponent} from "./team/team.component";
 import {TeamService} from "./services/team.service";
@@ -13,6 +13,7 @@ import {DialogboxComponent} from "./dialogbox/dialogbox.component";
 import {ProfileComponent} from "./profile/profile.component";
 import {LocationComponent} from "./location/location.component";
 import {LocationService} from "./services/userlocation.service";
+import {TypeAheadComponent} from "./type-ahead/type-ahead.component";
 
 
 @NgModule({
@@ -24,13 +25,15 @@ import {LocationService} from "./services/userlocation.service";
     PostComponent,
     DialogboxComponent,
     ProfileComponent,
-    LocationComponent
+    LocationComponent,
+    TypeAheadComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     ReactiveFormsModule,
+    JsonpModule,
     routing
   ],
   providers: [TeamService, LocationService],
---
+++ src/app/type-ahead/type-ahead.component.ts
@@ -0,0 +1,180 @@
+import { RequestOptionsArgs } from '@angular/http';
+import { Jsonp, URLSearchParams } from '@angular/http';
+import {
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  EventEmitter,
+  HostListener,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+  TemplateRef,
+  ViewChild,
+  ViewContainerRef
+} from '@angular/core';
+import { Observable, Subscription } from 'rxjs/Rx';
+
+enum Key {
+  Backspace = 8,
+  Tab = 9,
+  Enter = 13,
+  Shift = 16,
+  Escape = 27,
+  ArrowLeft = 37,
+  ArrowRight = 39,
+  ArrowUp = 38,
+  ArrowDown = 40
+}
+/*
+ using an external template:
+ <input [typeaheadTpl]="itemTpl" >
+ <template #itemTpl let-result>
+ <strong>MY {{ result.result }}</strong>
+ </template>
+ */
+@Component({
+  selector: '[typeahead]',
+  templateUrl: "./type-ahead.component.html",
+  styleUrls: ["./type-ahead.component.css"]
+})
+export class TypeAheadComponent implements OnInit, OnDestroy {
+  @Output() typeaheadSelected = new EventEmitter<string>();
+  @Input() typeaheadItemTpl: TemplateRef<any>;
+
+  private showSuggestions: boolean = false;
+  private results: string[];
+  private suggestionIndex: number = 0;
+  private subscriptions: Subscription[];
+  private activeResult: string;
+
+  @ViewChild('suggestionsTplRef') suggestionsTplRef;
+
+  @HostListener('keydown', ['$event'])
+  handleEsc(event: KeyboardEvent) {
+    if (event.keyCode === Key.Escape) {
+      this.hideSuggestions();
+      event.preventDefault();
+    }
+  }
+
+  constructor(private element: ElementRef,
+              private viewContainer: ViewContainerRef,
+              private jsonp: Jsonp,
+              private cdr: ChangeDetectorRef
+  ) { }
+
+  ngOnInit() {
+    this.subscriptions = [
+      this.filterEnterEvent(),
+      this.listenAndSuggest(),
+      this.navigateWithArrows()
+    ];
+    this.renderTemplate();
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
+    this.subscriptions.length = 0;
+  }
+
+  renderTemplate() {
+    this.viewContainer.createEmbeddedView(this.suggestionsTplRef);
+    this.cdr.markForCheck();
+  }
+
+  filterEnterEvent() {
+    return Observable.fromEvent(this.element.nativeElement, 'keydown')
+      .filter((e: KeyboardEvent) => e.keyCode === Key.Enter)
+      .subscribe((event: Event) => {
+        event.preventDefault();
+        this.handleSelectSuggestion(this.activeResult);
+      });
+  }
+
+  listenAndSuggest() {
+    return Observable.fromEvent(this.element.nativeElement, 'keyup')
+      .filter(this.validateKeyCode)
+      .map((e: any) => e.target.value)
+      .debounceTime(400)
+      .concat()
+      .distinctUntilChanged()
+      .filter((query: string) => query.length > 0)
+      .switchMap((query: string) => this.suggest(query))
+      .subscribe((results: string[]) => {
+        this.results = results;
+        this.showSuggestions = true;
+        this.cdr.markForCheck();
+      });
+  }
+
+  navigateWithArrows() {
+    return Observable.fromEvent(this.element.nativeElement, 'keydown')
+      .filter((e: any) => e.keyCode === Key.ArrowDown || e.keyCode === Key.ArrowUp)
+      .map((e: any) => e.keyCode)
+      .subscribe((keyCode: number) => {
+        let step = keyCode === Key.ArrowDown ? 1 : -1;
+        const topLimit = 9;
+        const bottomLimit = 0;
+        this.suggestionIndex += step;
+        if (this.suggestionIndex === topLimit + 1) {
+          this.suggestionIndex = bottomLimit;
+        }
+        if (this.suggestionIndex === bottomLimit - 1) {
+          this.suggestionIndex = topLimit;
+        }
+        this.showSuggestions = true;
+        // this.renderTemplate();
+        this.cdr.markForCheck();
+      });
+  }
+  suggest(query: string) {
+    const url = 'http://suggestqueries.google.com/complete/search';
+    const searchConfig: URLSearchParams = new URLSearchParams();
+    const searchParams = {
+      hl: 'en',
+      ds: 'yt',
+      xhr: 't',
+      client: 'youtube',
+      q: query,
+      callback: 'JSONP_CALLBACK'
+    };
+    Object.keys(searchParams).forEach(param => searchConfig.set(param, searchParams[param]));
+    const options: RequestOptionsArgs = {
+      search: searchConfig
+    };
+    return this.jsonp.get(url, options)
+      .map(response => response.json()[1])
+      .map(results => results.map(result => result[0]));
+  }
+
+  markIsActive(index: number, result: string) {
+    const isActive = index === this.suggestionIndex;
+    if (isActive) {
+      this.activeResult = result;
+    }
+    return isActive;
+  }
+  handleSelectSuggestion(suggestion: string) {
+    this.hideSuggestions();
+    this.typeaheadSelected.emit(suggestion);
+  }
+
+  validateKeyCode(event: KeyboardEvent) {
+    return event.keyCode !== Key.Tab
+      && event.keyCode !== Key.Shift
+      && event.keyCode !== Key.ArrowLeft
+      && event.keyCode !== Key.ArrowUp
+      && event.keyCode !== Key.ArrowRight
+      && event.keyCode !== Key.ArrowDown;
+  }
+
+  hideSuggestions() {
+    this.showSuggestions = false;
+  }
+
+  hasItemTemplate() {
+    return this.typeaheadItemTpl !== undefined;
+  }
+}
